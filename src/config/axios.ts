743b10a549b974db238b4761d3ab393c58bf58b2
Srikanth Cheruku <wpsrikanth@gmail.com>
2023-12-27 13:28:56 +0530

    first commit

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import { Axios } from "axios";
-import axios from 'axios';
+import axios from "axios";
 
 const instance: Axios = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
